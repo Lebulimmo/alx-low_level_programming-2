2498c987db1f9e5d12a729454601e0bcd25b319d
Lebulimmo <lebulimmo@gmail.com>
2023-06-18 11:32:58 -0700

    Variable

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+*main
+*Return Always 0
+*/
+
+int main (void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	
+		{
+			putchar(i);
+		}
+
+	putchar('\n');
+
+return(0); 
+}
